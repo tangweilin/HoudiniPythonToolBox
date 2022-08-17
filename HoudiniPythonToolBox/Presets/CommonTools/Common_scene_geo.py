62621b314f767e7fce35095f0f8cf2ecc3ab0eb9
tangweilin <t18686468297@163.com>
2022-08-17 16:01:11 +0800

    add doc

--- HoudiniPythonToolBox/Presets/CommonTools/Common_scene_geo.py
+++ HoudiniPythonToolBox/Presets/CommonTools/Common_scene_geo.py
@@ -2,7 +2,10 @@
 import hou
 
 
-def start_work():
+def start_work() -> None:
+    """
+        create a geo node in houdini obj pane
+    """
     plan = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
     pos = plan.selectPosition()
     up_node = plan.pwd()
@@ -10,4 +13,4 @@ def start_work():
         scene_geo_node = up_node.createNode('geo', 'scene_component')
         scene_geo_node.setPosition(pos)
     except:
-        pass
+        print("create scene node failed")
